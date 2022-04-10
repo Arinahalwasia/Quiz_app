5994add0497f0295cf62399749f7a5c100502de4
maddler-hub <Manjulmittal21@gmail.com>
2022-04-10 19:23:47 +0530

    Updating questions and style

---
+++ questions.js
@@ -0,0 +1,229 @@
+const questions =[
+ {
+   questionText:'Who is the Developer of Java?',
+   answerOptions:[
+    {answerText:'Dennis Ritchic',isCorrect:false},
+    {answerText:'James Gosling',isCorrect:true},
+    {answerText:'Bjarne Stroustrup',isCorrect:false},
+    {answerText:'Charles Babbage',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'What was Java Name Before?',
+   answerOptions:[
+    {answerText:'Java',isCorrect:false},
+    {answerText:'Javax',isCorrect:false},
+    {answerText:'classes & Java',isCorrect:false},
+    {answerText:'Oak',isCorrect:true},
+   ],
+ },
+ {
+   questionText:'In which year Java was developed?',
+   answerOptions:[
+    {answerText:'1991',isCorrect:true},
+    {answerText:'1972',isCorrect:false},
+    {answerText:'1979',isCorrect:false},
+    {answerText:'1995',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'What is the extension of Java?',
+   answerOptions:[
+    {answerText:'.gt',isCorrect:false},
+    {answerText:'.oak',isCorrect:false},
+    {answerText:'.java',isCorrect:true},
+    {answerText:'.exe',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'Java was developed by?',
+   answerOptions:[
+    {answerText:'Sun Micro Systems',isCorrect:true},
+    {answerText:'oracle',isCorrect:false},
+    {answerText:'Microsoft',isCorrect:false},
+    {answerText:'ANSI',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'Which of the following are flavours of Java?',
+   answerOptions:[
+    {answerText:'J2SE',isCorrect:false},
+    {answerText:'J2EE',isCorrect:false},
+    {answerText:'J2ME',isCorrect:false},
+    {answerText:'All of above',isCorrect:true},
+   ],
+ },
+ {
+   questionText:'Whose product is Java right now?',
+   answerOptions:[
+    {answerText:'Sun Micro Systems',isCorrect:false},
+    {answerText:'Microsoft',isCorrect:false},
+    {answerText:'ANSI',isCorrect:false},
+    {answerText:'Oracle',isCorrect:true},
+   ],
+ },
+ {
+   questionText:'What is the first version of Java?',
+   answerOptions:[
+    {answerText:'JDK alpha & Beta',isCorrect:true},
+    {answerText:'JDK 1.0',isCorrect:false},
+    {answerText:'JDK 15',isCorrect:false},
+    {answerText:'All of above',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'What is the compilation process of Java program?',
+   answerOptions:[
+    {answerText:'java class_name.java',isCorrect:false},
+    {answerText:'javac class_name.java',isCorrect:true},
+    {answerText:'javac class_name',isCorrect:false},
+    {answerText:'class_name.java',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'What is the execution process of Java program?',
+   answerOptions:[
+    {answerText:'java class_name',isCorrect:true},
+    {answerText:'javac class_name',isCorrect:false},
+    {answerText:'class_name.java',isCorrect:false},
+    {answerText:'class_name java',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'In java, JDK stands for?',
+   answerOptions:[
+    {answerText:'java Design kit',isCorrect:false},
+    {answerText:'java Developer kit',isCorrect:false},
+    {answerText:'java development kit',isCorrect:true},
+    {answerText:'none of these',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'In java,JRE stands for?',
+   answerOptions:[
+    {answerText:'java Running Environment',isCorrect:false},
+    {answerText:'java Runtime Environment',isCorrect:true},
+    {answerText:'java Runtime Execution',isCorrect:false},
+    {answerText:'None of these',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'In java JVM stands for?',
+   answerOptions:[
+    {answerText:'java Virtual Machine',isCorrect:true},
+    {answerText:'java Verification Machine',isCorrect:false},
+    {answerText:'java virtual Moment',isCorrect:false},
+    {answerText:'None of these',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'Which of the following is features of Java?',
+   answerOptions:[
+    {answerText:'Platform Independent',isCorrect:false},
+    {answerText:'Simple & Secure',isCorrect:false},
+    {answerText:'Dynamic',isCorrect:false},
+    {answerText:'All of above',isCorrect:true},
+   ],
+ },
+ {
+   questionText:'In java, System class is defined in?',
+   answerOptions:[
+    {answerText:'java.util package',isCorrect:false},
+    {answerText:'java.lang package',isCorrect:true},
+    {answerText:'java.io package',isCorrect:false},
+    {answerText:'none ',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'In java, Scanner class is used to take input at?',
+   answerOptions:[
+    {answerText:'Compile-time',isCorrect:false},
+    {answerText:'Run-time',isCorrect:true},
+    {answerText:'both a & b',isCorrect:false},
+    {answerText:'none',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'Which Streams is used for input?',
+   answerOptions:[
+    {answerText:'Scanner',isCorrect:false},
+    {answerText:'System',isCorrect:false},
+    {answerText:'System.out',isCorrect:false},
+    {answerText:'System.in',isCorrect:true},
+   ],
+ },
+ {
+   questionText:'Which scanner class method is used to read integer value?',
+   answerOptions:[
+    {answerText:'nextInt()',isCorrect:true},
+    {answerText:'next()',isCorrect:false},
+    {answerText:'readInt()',isCorrect:false},
+    {answerText:'nextInteger',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'How many types of Datatype in Java?',
+   answerOptions:[
+    {answerText:'One',isCorrect:false},
+    {answerText:'Two',isCorrect:true},
+    {answerText:'Three',isCorrect:false},
+    {answerText:'Four',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'What is the size of char Datatype?',
+   answerOptions:[
+    {answerText:'1 Byte',isCorrect:false},
+    {answerText:'2 Bytes',isCorrect:true},
+    {answerText:'4 Bytes',isCorrect:false},
+    {answerText:'none',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'Which of the following is smallest integer Datatype?',
+   answerOptions:[
+    {answerText:'int',isCorrect:false},
+    {answerText:'long',isCorrect:false},
+    {answerText:'byte',isCorrect:true},
+    {answerText:'short',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'Which of the following Datatypes comes under floating Datatypes?',
+   answerOptions:[
+    {answerText:'double',isCorrect:false},
+    {answerText:'long',isCorrect:false},
+    {answerText:'float',isCorrect:false},
+    {answerText:'Both a & c',isCorrect:true},
+   ],
+ },
+ {
+   questionText:'What is the Range of byte DataType is?',
+   answerOptions:[
+    {answerText:'-128 to 128',isCorrect:false},
+    {answerText:'-127 to 128',isCorrect:false},
+    {answerText:'-128 to 127',isCorrect:true},
+    {answerText:'-127 to 127',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'How many loops are their in java?',
+   answerOptions:[
+    {answerText:'Two',isCorrect:false},
+    {answerText:'Three',isCorrect:false},
+    {answerText:'Four',isCorrect:true},
+    {answerText:'None',isCorrect:false},
+   ],
+ },
+ {
+   questionText:'Which loop always execute atleast once,even condition is false?',
+   answerOptions:[
+    {answerText:'while loop',isCorrect:false},
+    {answerText:'do while loop',isCorrect:true},
+    {answerText:'for loop',isCorrect:false},
+    {answerText:'for-each loop',isCorrect:false},
+   ],
+ },
+];
+
+export default questions;
